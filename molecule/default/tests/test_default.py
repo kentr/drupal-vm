fdeb0baa3beefe064711158d569965c43c7990d6
Kent Richards <info@kentrichards.net>
2019-03-06 10:14:18 -0800

    Initial variables & test for `default` scenario

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,48 @@
 import os
 
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+# Define fixture for dynamic ansible role variables.
+# @see https://github.com/philpep/testinfra/issues/345#issuecomment-409999558
+@pytest.fixture
+def ansible_role_vars(host):
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    # Include variables from ansible variable files.
+    # Paths are relative to the scenario directory.
+    ansible_vars = host.ansible(
+        "include_vars",
+        ("file=../../default.config.yml"
+         " name=default_config"))["ansible_facts"]["default_config"]
+
+    ansible_vars.update(host.ansible(
+        "include_vars",
+        ("file=../../config.yml"
+         " name=config"))["ansible_facts"]["config"])
+
+    ansible_vars.update(host.ansible(
+        "include_vars",
+        ("file=../resources/prepare-vars.yml"
+         " name=prepare_vars"))["ansible_facts"]["prepare_vars"])
+
+    ansible_vars.update(host.ansible(
+        "include_vars",
+        ("file=../../local.config.yml"
+         " name=local_config"))["ansible_facts"]["local_config"])
+
+    return ansible_vars
+
+
+def test_installed_site_home_page_title(host, ansible_role_vars):
+
+    # Test both that the home page loads at all, and that
+    # it contains the configured site title.
+    cmd = host.run('curl -s --location ' +
+                   ansible_role_vars['local_domain'])
+
+    assert ('<title>Welcome to ' +
+            ansible_role_vars['drupal_site_name']) in cmd.stdout
