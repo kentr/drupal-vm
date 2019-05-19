d9b97d17cc3dccbbbed0d5670dc7858a8c1b6d74
Kent Richards <info@kentrichards.net>
2019-05-19 15:05:33 -0700

    Refactor dynamic variables loader
    Something changed upstream, and the included variables are no longer
    being stored in the `host` object.
    
    This moves the variables accumulation to the wrapper.
    
    Might be a hack, but I can't find the updtream change that caused this
    regression.

--- molecule/wordpress-deploy/tests/test_default.py
+++ molecule/wordpress-deploy/tests/test_default.py
@@ -3,6 +3,9 @@ import os
 import pytest
 import re
 import testinfra.utils.ansible_runner
+from ansible.utils.vars import combine_vars
+from ansible.template import Templar
+from ansible.parsing.dataloader import DataLoader
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
@@ -10,73 +13,84 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 # Define fixture for dynamic ansible role variables.
 @pytest.fixture
-def ansible_role_vars(host):
+def ansible_dynamic_vars(host):
 
     # Include variables from ansible variable files.
-    # Paths must be relative to the scenario directory.
-    # `test_variable_files` is declared in `base-config.yml`,
-    # under the `all` group.
+    # Paths must be relative to the directory from which the `molecule`
+    # command is run.
+    # `test_variable_files` is declared in `base-config.yml`, under
+    # the `all` group.
     try:
         vars_files = host.ansible.get_variables()['test_variable_files']
     except KeyError:
         raise KeyError("Could not get ansible variable 'test_variable_files'"
                        " to use in 'include_vars'. Is it defined?")
 
-    # Load variables from `vars_files` into `host`.
+    test_vars = {}
+
+    # Load variables from `vars_files`.
+    # Accumulating the variables in this scope might be a hack, but I
+    # can't find the updtream change that caused the previous code to
+    # stop working.
     for file in vars_files:
-        host.ansible(
+        test_vars = combine_vars(test_vars, host.ansible(
             "include_vars",
-            "file=" + file
-        )
+            "file={}".format(file)
+        )['ansible_facts'])
 
-    return host.ansible.get_variables()
+    # Interpolate the variables with the templater.
+    # See https://github.com/philpep/testinfra/issues/345#issuecomment-468814489 # noqa: E501
+    test_vars["ansible_play_host_all"] = testinfra_hosts
+    templar = Templar(loader=DataLoader(), variables=test_vars)
+
+    return templar.template(test_vars, fail_on_undefined=False)
 
 
-def test_installed_site_home_page_title(host, ansible_role_vars):
+def test_installed_site_home_page_title(host, ansible_dynamic_vars):
 
     # Test both that the home page loads at all, and that
     # it contains the configured site title.
     cmd = host.run('curl -s ' +
-                   ansible_role_vars['local_domain'])
+                   ansible_dynamic_vars['local_domain'])
 
-    assert '<title>' + ansible_role_vars['wp_site_title'] in cmd.stdout
+    assert '<title>' + ansible_dynamic_vars['wp_site_title'] in cmd.stdout
 
 
-def test_db_search_replace(host, ansible_role_vars):
+def test_db_search_replace(host, ansible_dynamic_vars):
 
     cmd = host.run('curl -s --location ' +
-                   ansible_role_vars['local_domain'] +
+                   ansible_dynamic_vars['local_domain'] +
                    '/test-page')
 
     assert ('search-replace test 1: http://' +
-            ansible_role_vars['local_domain']) in cmd.stdout
+            ansible_dynamic_vars['local_domain']) in cmd.stdout
 
     assert ('search-replace test 2: https://' +
-            ansible_role_vars['local_domain']) in cmd.stdout
+            ansible_dynamic_vars['local_domain']) in cmd.stdout
 
     assert ('search-replace test 3: http://' +
-            ansible_role_vars['local_domain']) in cmd.stdout
+            ansible_dynamic_vars['local_domain']) in cmd.stdout
 
     assert ('search-replace test 4: https://' +
-            ansible_role_vars['local_domain']) in cmd.stdout
+            ansible_dynamic_vars['local_domain']) in cmd.stdout
 
 
-def test_missing_files_redirect_present(host, ansible_role_vars):
+def test_missing_files_redirect_present(host, ansible_dynamic_vars):
 
     # Check status code for the `is-there.png` file.
     cmd = host.run('curl -s --head ' +
-                   ansible_role_vars['local_domain'] +
+                   ansible_dynamic_vars['local_domain'] +
                    '/wp-content/uploads/is-there.png')
 
     assert re.match('HTTP.*?200 OK', cmd.stdout)
 
 
-def test_missing_files_redirect_not_present(host, ansible_role_vars):
+def test_missing_files_redirect_not_present(host, ansible_dynamic_vars):
 
     cmd = host.run('curl -s --head ' +
-                   ansible_role_vars['local_domain'] +
+                   ansible_dynamic_vars['local_domain'] +
                    '/wp-content/uploads/not-there.png')
 
     assert re.match('HTTP.*?302 Found', cmd.stdout)
-    assert (ansible_role_vars['prod_domain'] +
+    assert (ansible_dynamic_vars['prod_domain'] +
             '/wp-content/uploads/not-there.png') in cmd.stdout
